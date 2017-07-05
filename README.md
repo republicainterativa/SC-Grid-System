41523d98822053b590017e96d291b54530b4c095
zergiocosta <sergio.costa@outlook.com>
2017-07-05 17:37:44 -0300

    1.0

---
+++ README.md
@@ -0,0 +1,33 @@
+# SC Grid System 
+#### Made with SASS 
+
+A "just add water" WordPress plugin in which you can manage column sizes with shortcodes
+It is a 12 columns Grid System (from 1 to 12, 12 being 100% width)
+
+
+## To specify the column size of your element, just follow the exemple bellow
+
+```css
+[col_6_of_12]
+	{{content}}
+[/col_6_of_12]
+/* If you want to use the 6-column grid, for example */
+
+[col_12_of_12]
+	{{content}}
+[/col_12_of_12]
+/* If you want to use the 12-column grid, for example */
+
+/* Pattern */
+[col_{n}_of_12]
+	{{content}}
+[/col_{n}_of_12]
+/* Where {n} is your column size from 1 to 12 and {{content}} could be whatever you want to show inside the column element */
+```
+
+### Download from WordPress plugins repository
+[SC GridSystem - You can download from the WordPress Plugins Repository](https://wordpress.org/plugins/sc-gridsystem/)
+
+
+## License
+[![WTFPL](wtfpl-badge.png "WTFPL")](https://github.com/zergiocosta/SC-GridSystem/blob/master/LICENSE)
---
+++ functions/shortcode.php
@@ -0,0 +1,74 @@
+<?php
+
+// Add Shortcode to display 12 column grid
+function sc_gs_12( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-12">' . $content . '</div>';
+}
+add_shortcode( 'col_12_of_12', 'sc_gs_12' );
+
+// Add Shortcode to display 11 column grid
+function sc_gs_11( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-11">' . $content . '</div>';
+}
+add_shortcode( 'col_11_of_12', 'sc_gs_11' );
+
+// Add Shortcode to display 10 column grid
+function sc_gs_10( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-12">' . $content . '</div>';
+}
+add_shortcode( 'col_10_of_12', 'sc_gs_10' );
+
+// Add Shortcode to display 9 column grid
+function sc_gs_9( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-9">' . $content . '</div>';
+}
+add_shortcode( 'col_9_of_12', 'sc_gs_9' );
+
+// Add Shortcode to display 8 column grid
+function sc_gs_8( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-8">' . $content . '</div>';
+}
+add_shortcode( 'col_8_of_12', 'sc_gs_8' );
+
+// Add Shortcode to display 7 column grid
+function sc_gs_7( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-7">' . $content . '</div>';
+}
+add_shortcode( 'col_7_of_12', 'sc_gs_7' );
+
+// Add Shortcode to display 6 column grid
+function sc_gs_6( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-6">' . $content . '</div>';
+}
+add_shortcode( 'col_6_of_12', 'sc_gs_6' );
+
+// Add Shortcode to display 5 column grid
+function sc_gs_5( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-5">' . $content . '</div>';
+}
+add_shortcode( 'col_5_of_12', 'sc_gs_5' );
+
+// Add Shortcode to display 4 column grid
+function sc_gs_4( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-4">' . $content . '</div>';
+}
+add_shortcode( 'col_4_of_12', 'sc_gs_4' );
+
+// Add Shortcode to display 3 column grid
+function sc_gs_3( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-3">' . $content . '</div>';
+}
+add_shortcode( 'col_3_of_12', 'sc_gs_3' );
+
+// Add Shortcode to display 2 column grid
+function sc_gs_2( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-2">' . $content . '</div>';
+}
+add_shortcode( 'col_2_of_12', 'sc_gs_2' );
+
+// Add Shortcode to display 1 column grid
+function sc_gs_1( $atts, $content = null ) {
+	return '<div class="sc-gs-col sc-gs-1">' . $content . '</div>';
+}
+add_shortcode( 'col_1_of_12', 'sc_gs_1' );
+
